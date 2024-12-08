3940f3c56ba24a702b0381a087cd6acebc6374f3
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2024-12-08 21:23:49 +0300

    Add files via upload

--- utils/imports.py
+++ utils/imports.py
@@ -1,18 +1,16 @@
-import pip, os, re, random, importlib
+import pip, os, re, random, importlib, subprocess
 try:
-	from git import Repo
 	from pyrogram.handlers import MessageHandler
 	from pyrogram import Client, filters, idle, errors
 	from pyrogram.raw.functions.account import GetAuthorizations, DeleteAccount
 	from pyrogram.types import ChatPermissions
 	from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 	from gtts import gTTS
-	import sqlite3, logging, time, requests, asyncio, pyttsx3, datetime, random, json, g4f, html, psutil, shutil, platform, sys, pyfiglet
+	import sqlite3, logging, time, requests, asyncio, pyttsx3, datetime, random, json, g4f, html, psutil, shutil, platform, sys, pyfiglet, fade
 	from datetime import datetime, timedelta
 	from time import gmtime, strptime, strftime
 	from io import StringIO
 	from contextlib import redirect_stdout
-	import subprocess
 	import speech_recognition as sr
 	from pydub import AudioSegment
 	from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -24,5 +22,5 @@ try:
 	from colorama import Fore, Style
 except Exception as e:
 	print(e)
-	pip.main(['install', '-r', 'requirements.txt'])
+	subprocess.run("python3 -m pip install -r requirements.txt", shell=True, capture_output=True)
 	os.execvp(sys.executable, [sys.executable, "main.py"])
