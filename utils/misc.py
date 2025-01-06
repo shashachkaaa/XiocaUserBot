a6c524fae97e79b41c1da45360888aef58c27e3e
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2025-01-06 14:13:05 +0500

    Add files via upload

--- utils/misc.py
+++ utils/misc.py
@@ -6,6 +6,6 @@ allowed = db.get("core.main", "allow")
 print(db.get("core.main", "allowed"))
 system = platform.system()
 start_time = time.time()
-META_COMMENTS = re.compile(r"^ *# *meta +(\S+) *: *(.*?)\s*$", re.MULTILINE)
+meta_comments = re.compile(r"^ *# *meta +(\S+) *: *(.*?)\s*$", re.MULTILINE)
 req_list = []
 modules_help = {}
